1e9ab891f3cbf127c33d39c2718827b584c0778b
Martin Rivero <rivero.info@gmail.com>
2024-12-07 17:20:53 -0300

    add more compute

--- terraform/main.tf
+++ terraform/main.tf
@@ -199,9 +199,9 @@ resource "aws_eks_node_group" "node_group_obligatorio" {
   subnet_ids = [aws_subnet.subnet_obligatario_public_1.id, aws_subnet.subnet_obligatario_public_2.id]
 
   scaling_config {
-    desired_size = "2"
-    min_size     = "2"
-    max_size     = "5"
+    desired_size = "4"
+    min_size     = "4"
+    max_size     = "10"
   }
 
   instance_types = var.instance_types
